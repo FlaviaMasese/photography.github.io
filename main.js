7ec3720a15f153ebf750e894bd80da07d8b5caa6
MrsOye <flaviabosibori@gmail.com>
2024-01-24 06:57:24 +0100

    files added

---
+++ main.js
@@ -0,0 +1,65 @@
+(function ($) {
+    "use strict";
+    
+    // Initiate the wowjs animation library
+    new WOW().init();
+
+    // Initiate nav menu
+    $('.menu, .overlay').click(function () {
+        $('.menu').toggleClass('clicked');
+        $('#nav').toggleClass('show');
+    });
+    
+    //Portfolio modal slider
+    $('.port-slider').slick({
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.port-slider-nav'
+    });
+    $('.port-slider-nav').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.port-slider',
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+    
+    $('#popover-content-download').hide();
+    $("[data-toggle=popover]").each(function (e) {
+        $(this).popover({
+            html: true,
+            content: function () {
+                var id = $(this).attr('id')
+                return $('#popover-content-' + id).html();
+            }
+        });
+
+    });
+	
+	// Date and time picker of booking section
+    $('#date-1, #date-2').datetimepicker({
+        format: 'L'
+    });
+    $('#time-1, #time-2').datetimepicker({
+        format: 'LT'
+    });
+})(jQuery);
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+//contact buttons (circulartext buttons)
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text= textButton.quarySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) =>`<span style="transform: rotate($
+    {index * 12}deg">${character}</span)>`).join('')
+})
+
